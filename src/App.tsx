79a127a87f566c620c1ff52ecb214d93509ebd8e
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2023-07-01 13:27:30 +0330

    solved challenge 27 Remove Element

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
         <ol>
           <li>1. two sums </li>
           <li>88. Merge Sorted Array </li>
+          <li>27. Remove Element</li>
         </ol>
       </div>
     </>
