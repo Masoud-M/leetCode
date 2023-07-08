c58bef9ad29d53bf928fccc00a7ab4f7ceb898c2
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2023-07-08 14:55:38 +0330

    passed the challenge 27

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
           <li>1. two sums </li>
           <li>88. Merge Sorted Array </li>
           <li>27. Remove Element</li>
+          <li>26. Remove Duplicates from Sorted Array</li>
         </ol>
       </div>
     </>
