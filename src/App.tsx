ba9cf094f4f16da1e1e837ae82a419b0312ad32d
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2023-07-14 23:12:24 +0330

    solved challenge 169. Majority Element

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
           <li>27. Remove Element</li>
           <li>26. Remove Duplicates from Sorted Array</li>
           <li>80. Remove Duplicates from Sorted Array II</li>
+          <li>169. Majority Element</li>
         </ol>
       </div>
     </>
